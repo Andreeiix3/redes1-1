882920c7ed98f585ba9424baf0fbc25f33532717
emiliocuestaf <emiliocuestaf@gmail.com>
2017-11-14 02:26:22 +0100

    incomprensible

--- p3/practica.bash
+++ p3/practica.bash
@@ -27,12 +27,12 @@ tshark -r trazap3.pcap -T fields -e frame.len -e eth.type -e vlan.etype -e ip.sr
 echo -e "##### APARTADO 1: PORCENTAJE DE PAQUETES IP #####\n"
 awk 'BEGIN{FS="\t";}
 {	
-	if (($2 == 2048)||($3 == 2048)){
- 		n_ip = n_ip + 1;
- 		if($14 == 6)
- 			n_tcp = n_tcp + 1;
- 		if($14 == 17)
- 			n_udp = n_udp + 1;
+	if ($2 == 2048 || $3 == 2048 ){
+		n_ip = n_ip + 1;
+ 		if($14 == 6){
+ 			n_tcp = n_tcp + 1;}
+ 		if($14 == 17){
+ 			n_udp = n_udp + 1;}
  	}	
 }
 END {
@@ -42,9 +42,9 @@ END {
 	udp = n_udp/n_ip*100;
     printf "\t*El porcentaje de paquetes IP es  %.4f %%\n", 100*data;
 	print "\t De los cuales:";
-	printf "\t\t*El %.4f %% son TCP", tcp;
-	printf "\t\t*El  %.4f %% son UDP", udp;
-	printf "\t\t*El  %.4f %% son OTROS", 100-tcp-udp; 
+	printf "\t\t*El %.4f %% son TCP\n", tcp;
+	printf "\t\t*El %.4f %% son UDP\n", udp;
+	printf "\t\t*El %.4f %% son OTROS\n", 100-tcp-udp; 
 	printf "\t*El porcentaje de paquetes NO-IP es %.4f %%\n", 100*(1-data);
 	
 }' datos.txt
